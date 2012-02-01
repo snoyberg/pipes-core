4c460bcde7ae3471db6b5394b56e4142c999b2b2
Paolo Capriotti <p.capriotti@gmail.com>
2012-02-01 15:46:37 +0000

    Use Unit instead of Void to plug the left end of a Pipeline.

--- Control/Pipe/Common.hs
+++ Control/Pipe/Common.hs
@@ -154,13 +154,13 @@ instance (Monad m) => Monad (Pipe a b m) where
 instance MonadTrans (Pipe a b) where lift = M . liftM pure
 
 -- | A pipe that can only produce values
-type Producer b m r = Pipe Void b m r
+type Producer b m r = Pipe () b m r
 
 -- | A pipe that can only consume values
 type Consumer a m r = Pipe a Void m r
 
 -- | A self-contained pipeline that is ready to be run
-type Pipeline m r = Pipe Void Void m r
+type Pipeline m r = Pipe () Void m r
 
 {-|
     Wait for input from upstream within the 'Pipe' monad:
@@ -248,6 +248,6 @@ runPipe p' = case p' of
     Pure r          -> return r
     M mp            -> mp >>= runPipe
     -- Technically a blocked Pipe can still await
-    Await f         -> runPipe $ f undefined
+    Await f         -> runPipe $ f (Just ())
     -- A blocked Pipe can not yield, but I include this as a precaution
     Yield _ p       -> runPipe p
