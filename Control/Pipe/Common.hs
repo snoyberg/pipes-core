8a7c0fda7a47de5ba17255dec3fb4dc906643af7
Paolo Capriotti <p.capriotti@gmail.com>
2012-02-29 01:57:39 +0000

    Simplify handler definition.

--- Control/Pipe/Common.hs
+++ Control/Pipe/Common.hs
@@ -11,13 +11,14 @@ module Control.Pipe.Common (
   catchP,
   catch,
   catchM,
-  catch_,
   onException,
   finally,
   bracket,
   await,
   yield,
   lift,
+  masked,
+  ensure,
   MaskState(..),
   lift_,
   tryAwait,
@@ -52,7 +53,7 @@ data BrokenUpstreamPipe = BrokenUpstreamPipe
 
 instance Exception BrokenUpstreamPipe
 
-data MaskState = Masked | Unmasked | Strict
+data MaskState = Masked | Unmasked | Ensure
 
 data PipeF a b m x
   = M (m x) MaskState
@@ -67,7 +68,7 @@ instance Monad m => Functor (PipeF a b m) where
 data Pipe a b m r
   = Pure r
   | Free (PipeF a b m (Pipe a b m r))
-         (SomeException -> m (Pipe a b m r))
+         (SomeException -> Pipe a b m r)
   | Throw SomeException
 
 type Producer b m = Pipe () b m
@@ -78,7 +79,7 @@ instance Monad m => Monad (Pipe a b m) where
   return = Pure
   Pure r >>= f = f r
   Free c h >>= f = Free (fmap (>>= f) c)
-                        (h >=> \p -> return $ p >>= f)
+                        (h >=> f)
   Throw e >>= _ = Throw e
 
 instance Monad m => Functor (Pipe a b m) where
@@ -89,46 +90,38 @@ instance Monad m => Applicative (Pipe a b m) where
   (<*>) = ap
 
 liftF :: Monad m => PipeF a b m r -> Pipe a b m r
-liftF c = Free (fmap return c) (return . throw)
+liftF c = Free (fmap return c) throw
 
 catch :: (Monad m, Exception e)
       => Pipe a b m r
-      -> (e -> m (Pipe a b m r))
+      -> (e -> Pipe a b m r)
       -> Pipe a b m r
 catch p h = catchP p $ \e -> case E.fromException e of
-  Nothing -> return $ throw e
+  Nothing -> throw e
   Just e' -> h e'
 
 catchM :: (Monad m, Exception e)
        => Pipe a b m r
        -> (e -> m r)
        -> Pipe a b m r
-catchM p h = catch p (liftM return . h)
-
-catch_ :: (Monad m, Exception e)
-       => Pipe a b m r
-       -> (e -> Pipe a b m r)
-       -> Pipe a b m r
-catch_ p h = catch p (return . h)
+catchM p h = catch p $ \e -> ensure (h e)
 
 throw :: (Monad m, Exception e) => e -> Pipe a b m r
 throw e = Throw . E.toException $ e
 
 onException :: Monad m
             => Pipe a b m r
-            -> m (Pipe a b m s)
+            -> Pipe a b m s
             -> Pipe a b m r
-onException p w = catchP p h
-  where
-    h e = w >>= \p' -> return $ p' >> throw e
+onException p w = catchP p $ \e -> w >> throw e
 
 finally :: Monad m
         => Pipe a b m r
         -> m s
         -> Pipe a b m r
 finally p w = do
-  r <- onException p (liftM return w)
-  lift_ Strict w
+  r <- onException p (ensure w)
+  ensure w
   return r
 
 bracket :: Monad m
@@ -138,24 +131,23 @@ bracket :: Monad m
         -> Pipe a b m x
 bracket open close run = do
   r <- lift_ Masked open
-  x <- onException (run r) (liftM return (close r))
-  lift_ Strict $ close r
+  x <- onException (run r) (ensure (close r))
+  ensure $ close r
   return x
 
 catchP :: Monad m
        => Pipe a b m r
-       -> (SomeException -> m (Pipe a b m r))
+       -> (SomeException -> Pipe a b m r)
        -> Pipe a b m r
 catchP (Pure r) _ = return r
-catchP (Free c h1) h2 = Free c $ \e ->
-  h1 e >>= \p -> return $ catchP p h2
-catchP (Throw e) h = Free (M (h e) Strict) (return . throw)
+catchP (Free c h1) h2 = Free c $ \e -> catchP (h1 e) h2
+catchP (Throw e) h = h e
 
 await :: Monad m => Pipe a b m a
 await = liftF $ Await id
 
 tryAwait :: Monad m => Pipe a b m (Maybe a)
-tryAwait = catch_ (liftM Just await) $ \(_ :: BrokenUpstreamPipe) -> return Nothing
+tryAwait = catch (liftM Just await) $ \(_ :: BrokenUpstreamPipe) -> return Nothing
 
 yield :: Monad m => b -> Pipe a b m ()
 yield x = liftF $ Yield x ()
@@ -165,6 +157,12 @@ lift_ s m = liftF (M m s)
 
 instance MonadTrans (Pipe a b) where
   lift = lift_ Unmasked
+
+masked :: Monad m => m r -> Pipe a b m r
+masked = lift_ Masked
+
+ensure :: Monad m => m r -> Pipe a b m r
+ensure = lift_ Ensure
 
 pipe :: Monad m => (a -> b) -> Pipe a b m r
 pipe f = forever $ await >>= yield . f
@@ -185,7 +183,7 @@ compose :: Monad m
    -> PipeF b c m y
    -> Composition a b c m x y
 compose (Yield b x) (Await k) = AdvanceBoth x (k b)
-compose (M m Strict) _ = AdvanceFirst (lift_ Strict m)
+compose (M m Ensure) _ = AdvanceFirst (lift_ Ensure m)
 compose _ (Yield c y) = AdvanceSecond (yield c >> return y)
 compose _ (M m s) = AdvanceSecond (lift_ s m)
 compose (M m s) _ = AdvanceFirst (lift_ s m)
@@ -195,21 +193,15 @@ infixl 9 >+>
 (>+>) :: Monad m => Pipe a b m r -> Pipe b c m r -> Pipe a c m r
 p1 >+> p2 = case (p1, p2) of
   (Free c1 h1, Free c2 h2) -> case compose c1 c2 of
-    AdvanceFirst comp -> catchP comp (liftH h1) >>= \p1' -> p1' >+> p2
-    AdvanceSecond comp -> catchP comp (liftH h2) >>= \p2' -> p1 >+> p2'
+    AdvanceFirst comp -> catchP comp (return . h1) >>= \p1' -> p1' >+> p2
+    AdvanceSecond comp -> catchP comp (return . h2) >>= \p2' -> p1 >+> p2'
     AdvanceBoth p1' p2' -> p1' >+> p2'
-  (Free _ h, Throw e) ->
-    lift_ Strict (h e) >>= \p1' -> p1' >+> p2
-  (Throw e, Free _ h) ->
-    lift_ Strict (h e) >>= \p2' -> p1 >+> p2'
-  (Free _ h, Pure _) ->
-    lift_ Strict (h (E.toException BrokenDownstreamPipe)) >>= \p1' -> p1' >+> p2
-  (Pure _, Free _ h) ->
-    lift_ Strict (h (E.toException BrokenUpstreamPipe)) >>= \p2' -> p1 >+> p2'
+  (Free _ h, Throw e) -> h e >+> p2
+  (Throw e, Free _ h) -> p1 >+> h e
+  (Free _ h, Pure _) -> h (E.toException BrokenDownstreamPipe) >+> p2
+  (Pure _, Free _ h) -> p1 >+> h (E.toException BrokenUpstreamPipe)
   (_, Throw e) -> throw e
   (_, Pure r) -> return r
-  where
-    liftH h e = h e >>= \p' -> return (return p')
 
 infixr 9 <+<
 (<+<) :: Monad m => Pipe b c m r -> Pipe a b m r -> Pipe a c m r
@@ -222,7 +214,7 @@ runPipe p = E.mask $ \restore -> run restore p
       where
         go (Pure r) = return r
         go (Free c h) = stepPipe try c >>= \x -> case x of
-          Left e   -> h e >>= go
+          Left e   -> go $ h e
           Right p' -> go p'
         go (Throw e) = E.throwIO e
 
@@ -233,9 +225,7 @@ runPipe p = E.mask $ \restore -> run restore p
 runPurePipe :: Monad m => Pipeline m r -> m (Either SomeException r)
 runPurePipe (Pure r) = return $ Right r
 runPurePipe (Throw e) = return $ Left e
-runPurePipe (Free c h) = stepPipe try c >>= \x -> case x of
-  Left e   -> h e >>= runPurePipe
-  Right p' -> runPurePipe p'
+runPurePipe (Free c h) = stepPipe try c >>= runPurePipe . either h id
   where try m _ = liftM Right m
 
 runPurePipe_ :: Monad m => Pipeline m r -> m r
